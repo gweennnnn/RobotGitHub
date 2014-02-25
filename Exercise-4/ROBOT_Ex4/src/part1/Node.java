0252cf419e738a0b6adbc759d544fcbbff71abe8
gweennnnn <me@gwendolyn.info>
2014-02-25 17:53:24 +0000

    Edits to Node
    Added Getter Setter, fixed getPredecessor, change values to private

--- Exercise-4/ROBOT_Ex4/src/part1/Node.java
+++ Exercise-4/ROBOT_Ex4/src/part1/Node.java
@@ -5,8 +5,8 @@ import java.util.ArrayList;
 import rp13.search.util.ActionStatePair;
 
 public class Node<_action, _state> {
-	Node predecessor;
-	ActionStatePair<_action, _state> data;
+	private Node predecessor;
+	private ActionStatePair<_action, _state> data;
 	
 	public Node(ActionStatePair<_action, _state> data) {
 		this(data, null);
@@ -24,16 +24,24 @@ public class Node<_action, _state> {
 		list = new ArrayList<_action>();
 
 		
-		if (predecessor == null) 
-		{
-			return list;
-		}
-		else 
+		if (predecessor != null)
 		{
 			list = predecessor.solutionList();
 			list.add(data.getAction());
 		}
 		
 		return list;
+	}
+
+	public Node getPredecessor() {
+		return predecessor;
+	}
+
+	public ActionStatePair<_action, _state> getData() {
+		return data;
+	}
+
+	public void setData(ActionStatePair<_action, _state> data) {
+		this.data = data;
 	}
 }
