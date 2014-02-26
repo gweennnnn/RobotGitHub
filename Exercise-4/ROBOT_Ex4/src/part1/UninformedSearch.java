9b7c62d03dc341db58155f83db56149f00016909
biego <gxb319@bham.ac.uk>
2014-02-26 15:02:43 +0000

    Updated Uniformed search
    Changed it from recursion to a while loop which might solve a few
    problems?

--- Exercise-4/ROBOT_Ex4/src/part1/UninformedSearch.java
+++ Exercise-4/ROBOT_Ex4/src/part1/UninformedSearch.java
@@ -16,14 +16,17 @@ import rp13.search.util.EqualityGoalTest;
 public class UninformedSearch<_action, _state> extends EqualityGoalTest<_state>
 {
 	private _state startState;
-	private Agenda<Node<_action, _state>> list;
+	private _action startAction;
+	private Agenda<Node<_action, _state>> list; 
 	
 	public enum SearchType { DepthFirst, BreadthFirst };
 	
-	public UninformedSearch(_state startState, _state endState, SearchType searchType)
+	public UninformedSearch(_state startState, _action startAction, _state endState, SearchType searchType)
 	{
 		super(endState);
 		this.startState = startState;
+		this.startAction = startAction;
+		
 		if(searchType.equals(SearchType.DepthFirst))
 			this.list = new Queue<_action, _state>();
 		else
@@ -41,33 +44,81 @@ public class UninformedSearch<_action, _state> extends EqualityGoalTest<_state>
 
 	public List<_action> search(Node<_action, _state> currNode, SuccessorFunction<_action, _state> succFunct, List<ActionStatePair<_action, _state>> successors)
 	{
-		//goda your code
-		//this has to be recursive so i added everything from your previous while loop
-		if(this.isGoal(currNode.getState()))
-			return currNode.solutionList();
+//		//Why does this have to be recursive?
+//		//goda your code
+//		//this has to be recursive so i added everything from your previous while loop
+//		if(this.isGoal(currNode.getState()))
+//			return currNode.solutionList();
+//		
+//		succFunct.getSuccessors(currNode.getState(), successors);
+//		
+//		for(ActionStatePair<_action, _state> successor : successors)
+//		{
+//			if(!this.list.contains(new Node<_action, _state>(successor.getAction(), successor.getState()))) // <---doesn't this check only in successors list? when it should check in the checked list?
+//			{
+//				Node<_action, _state> node = new Node<_action, _state>(successor.getAction(), successor.getState());
+//				
+//				this.list.push(node);
+//			}
+//		}
+//		
+//		
+//		if(this.getList().isEmpty()) // <----if successor list is empty
+//			return null;
+//		else
+//		{
+//			Node<_action, _state> tempNode = this.list.pop();
+//			search(tempNode, succFunct, new ArrayList<ActionStatePair<_action, _state>>());
+//		}
+//		
+//		return null;
 		
-		succFunct.getSuccessors(currNode.getState(), successors);
+		List<_action> failed = new ArrayList<_action>();
 		
-		for(ActionStatePair<_action, _state> successor : successors)
-		{
-			if(!this.list.contains(new Node<_action, _state>(successor.getAction(), successor.getState())))
-			{
-				Node<_action, _state> node = new Node<_action, _state>(successor.getAction(), successor.getState());
+		List<ActionStatePair<_action, _state>> successorsList = new ArrayList<ActionStatePair<_action, _state>>();
+		
+		Node<_action, _state> currentNode = new Node<_action, _state>(startAction, startState);
+		
+		_state currentState = currNode.getState();
+		
+		
+		
+
+		
+//		frontier.add(currentNode);
+		this.list.push(currNode);
+		
+//			while(!frontier.isEmpty()){
+			while(!this.list.isEmpty()){
 				
-				this.list.push(node);
+			
+				if(this.isGoal(currentState)){
+//					return currentNode.solutionList();
+					return currNode.solutionList();
+				}
+				//FILLING IN THE FRONTIERS LIST WITH SUCCESSORS
+				
+				succFunct.getSuccessors(currentState, successorsList); //<----------gets all the successors
+				
+				for(ActionStatePair<_action, _state> state : successorsList){ //<--------------saves all the successors as nodes
+					
+					if(!list.contains(currNode)){
+						
+						Node<_action, _state> node = new Node<_action, _state>(state.getAction(), state.getState(), currNode);
+						
+						this.list.push(node); //<------------------adds it to the frontiers list
+					}
+
+					
+				}
+				
+				currNode = this.list.pop();		//<------already adds an item to the explored set	
+				currentState = currNode.getState();
 			}
+			
+			return failed;
+			
 		}
-		
-		
-		if(this.getList().isEmpty())
-			return null;
-		else
-		{
-			Node<_action, _state> tempNode = this.list.pop();
-			search(tempNode, succFunct, new ArrayList<ActionStatePair<_action, _state>>());
-		}
-		
-		return null;
 	}
 	
 	public static void main (String[] args)
