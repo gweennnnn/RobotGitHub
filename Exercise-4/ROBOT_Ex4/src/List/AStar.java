79fc0752f8592bcdec18bf7a7bc51fedbfc7592e
gweennnnn <me@gwendolyn.info>
2014-02-27 17:17:37 +0000

    clean up 2
    :D

--- Exercise-4/ROBOT_Ex4/src/List/AStar.java
+++ Exercise-4/ROBOT_Ex4/src/List/AStar.java
@@ -1,8 +1,23 @@
 package List;
 
-import part1.Node;
 import rp13.search.interfaces.Agenda;
 
-public class AStar <_action, _state> extends ListType<_action, _state>{
+public class AStar <_action, _state> extends ListType<_action, _state>
+{
+	public AStar()
+	{
+		super();
+	}
+	
+	@Override
+	public void push(Node<_action, _state> _item) {
+		frontier.add(_item);
+	}
+
+	@Override
+	public Node<_action, _state> pop() {
+		System.out.println("Extend this class and overwrite this particular method");
+		return null;
+	}
 
 }
