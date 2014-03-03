cf37964a9f3de87d90ff5406d01a594fa01e0b4b
gweennnnn <me@gwendolyn.info>
2014-03-03 18:34:50 +0000

    Goda's changes :D

--- Exercise-4/ROBOT_Ex4/src/List/AStar.java
+++ Exercise-4/ROBOT_Ex4/src/List/AStar.java
@@ -1,33 +1,33 @@
-//package List;
-//
-//import rp13.search.interfaces.Agenda;
-//
-//public class AStar <_action, _state> extends ListType<_action, _state>
-//{
-//	public AStar()
-//	{
-//		super();
-//	}
-//	
-//	@Override
-//	public void push(AStarNode<_action, _state> _item) {
-//		frontier.add(_item);
-//	}
-//
-//	@Override
-//	public AStarNode<_action, _state>  pop() {
-//		System.out.println("Extend this class and overwrite this particular method");
-//		return null;
-//	}
-//
-//}
-
-//ALL THIS CLASS SHOULD DO IS CALCULATE THE VALUES FOR THE ASTARNODE
-//THIS USES A SORTING ALGORITHM AND RETURNS A SORTED FRONTIER SET
-
 package List;
 
-public class AStar
+public class AStar <_action, _state> extends ListType<_action, _state>
 {
+	public AStar()
+	{
+		super();
+	}
 	
+	public void push(AStarNode<_action, _state> item) {
+		boolean added = false;
+		if(frontier.size() == 0)
+			frontier.add(item);
+		else
+		{
+			for(int i = 0; i < frontier.size(); i++)
+			{
+				if(item.getValue() <= frontier.get(i).getValue())
+				{
+					frontier.add(i, item);
+					added = true;
+				}
+			}
+			if (added == false) frontier.add(item);
+		}
+	}
+	
+	@Override
+	public AStarNode<_action, _state>  pop() {
+		System.out.println("Extend this class and overwrite this particular method");
+		return null;
+	}
 }
